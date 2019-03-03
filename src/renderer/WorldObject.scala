9531b9bc67b1f870ff9808fbfce9e5a3652f32a0
Miska Kananen <miska.kananen@gmail.com>
2019-03-03 14:57:23 +0200

    Add tests for world matrix

--- src/renderer/WorldObject.scala
+++ src/renderer/WorldObject.scala
@@ -2,9 +2,15 @@ package renderer
 
 class WorldObject(val model: Model, var worldMatrix: Matrix4) {
   
+}
+
+object WorldObject {
+  
   // build a matrix that maps the model from model space
   // to desired position and orientation in world space 
-  def buildWorldMatrix(position: Vector4, orientation: Vector4) = {
+  def buildWorldMatrix(position: Vector4, orientation: Vector4) : Matrix4 = {
     ???
   }
+  
+  def buildWorldMatrix(position: Vector4) : Matrix4 = buildWorldMatrix(position, Vector4(0, 0, 1))
 }
---
+++ src/tests/WorldObjectTest.scala
@@ -0,0 +1,45 @@
+package tests
+
+import org.junit.Test
+import org.junit.Assert._
+
+import renderer.Vector4
+import renderer.WorldObject
+
+class WorldObjectTest {
+  
+  @Test def worldMatrixTranslation() = {
+    val v1 = Vector4(0, 0, 0)
+    val v2 = Vector4(3, -1, 2)
+    
+    val worldMatrix = WorldObject.buildWorldMatrix(Vector4(1, 2, 3))
+    
+    val v1t = worldMatrix * v1
+    val v2t = worldMatrix * v2
+    
+    assertEquals(Vector4(1, 2, 3), v1t)
+    assertEquals(Vector4(4, 1, 5), v2t)
+  }
+  
+  @Test def worldMatrixRotation() = {
+    val v1 = Vector4(0, 0, 0)
+    val v2 = Vector4(0, 0, 2)
+    val v3 = Vector4(3, 0, 0)
+    val v4 = Vector4(0, 1, 0)
+    
+    val worldMatrix = WorldObject.buildWorldMatrix(Vector4(0, 0, 0), Vector4(0, 1, 0))
+    
+    assertEquals(Vector4(0, 0, 0), worldMatrix * v1)
+    assertEquals(Vector4(0, 2, 0), worldMatrix * v2)
+    assertEquals(Vector4(3, 0, 0), worldMatrix * v3)
+    assertEquals(Vector4(0, 0, -1), worldMatrix * v4)
+  }
+  
+  @Test def worldMatrixTranslationRotation() = {
+    val v1 = Vector4(1, 2, 3)
+    
+    val worldMatrix = WorldObject.buildWorldMatrix(Vector4(4, 5, 6), Vector4(0, -1, 0))
+    
+    assertEquals(Vector4(5, 5, 6), worldMatrix * v1)
+  }
+}
