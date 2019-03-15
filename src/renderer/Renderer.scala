126e58a075840cfb83209e199719713f30aca931
Miska Kananen <miska.kananen@gmail.com>
2019-03-15 12:02:54 +0200

    Add projection matrix

--- src/renderer/Renderer.scala
+++ src/renderer/Renderer.scala
@@ -5,10 +5,17 @@ import scala.math.min
 
 class Renderer {
   
-  def buildProjectionMatrix(near: Double, far: Double, fov: Double) = ???
+  def buildProjectionMatrix(near: Double, far: Double) = {
+    new Matrix4(Array(Array(1, 0, 0, 0), Array(0, 1, 0, 0), 
+        Array(0, 0, (far+near)/(far-near), -(2*far*near)/(far-near)), Array(0, 0, 1, 0)))
+  }
   
   def render(world: World) : Array[Triangle] = {
-    ???
+    val clipSpaceTrg = toClipSpace(world)
+    
+    // do triangle clipping here
+    
+    return clipSpaceTrg
   }
   
   private def closestVertexDist(trg: Triangle) : Double = {
@@ -20,7 +27,7 @@ class Renderer {
   }
   
   private def toClipSpace(world: World) : Array[Triangle] = {
-    val projMatrix = buildProjectionMatrix(0.1, 100.0, 90.0)
+    val projMatrix = buildProjectionMatrix(0.1, 100.0)
     val viewMatrix = world.camera.buildViewMatrix()
     
     val projected = Buffer[Triangle]()
